68c88d209cfe7f6e07150d00eb85487096b2b8c7
Илья Колдунов <gigafiga21@gmail.com>
2017-11-20 15:11:05 +0200

    Added base EventQueue functional (issue #3).

---
+++ Libraries/Events/eventqueue.cpp
@@ -0,0 +1,29 @@
+#ifdef EVENTQUEUE_H
+
+    using namespace std;
+
+    class EventQueue
+    {
+        private:
+            List<void (*)(int)>* lQueue;
+        public:
+            void addListener(void (*listener)(int))
+            {
+                this->lQueue->push(listener);
+            }
+            void run()
+            {
+                ListUnit<void (*)(int)>* luCurrent = this->lQueue->first;
+                while (luCurrent != NULL)
+                {
+                    luCurrent->element(1);
+                    luCurrent = luCurrent->getNext();
+                }
+            }
+            EventQueue()
+            {
+                lQueue = new List<void (*)(int)>();
+            }
+    };
+
+#endif
